1be0326bebaade9943b5c6cbedbfb4991264de11
NovelF <93209668+NovelF@users.noreply.github.com>
2024-04-25 15:55:10 +0200

    Create chpass.sh

---
+++ chpass.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./elasticsearch-setup-passwords interactive <<EOF
+y
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+elastic
+EOF
